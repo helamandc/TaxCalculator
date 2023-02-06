dbb28b2be6fdaa3b7523b8da58835904a54bbd33
Helaman Del Castillo <helaman.delcastillo@praemium.com>
2023-02-06 14:46:00 +1100

    Add project files.

---
+++ CalculatingTax/CalculateIncomeTax.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatingTax
+{
+    public class CalculateIncomeTax
+    {
+        
+        public double grossSalary;
+        public double netSalary;
+        public double taxPercentage = 0.10;
+
+        public double TotalSalary(double gross)
+        {
+            netSalary = gross - (gross * taxPercentage);
+            return netSalary;
+        }
+
+        [Test]
+        public void TestTaxCalculator()
+        {
+            //Arrange
+            grossSalary = 100.00;
+            //Act
+            netSalary = grossSalary - (grossSalary * taxPercentage);
+            //Assert
+            Assert.AreEqual(90, netSalary);
+
+
+        }
+
+    }
+}
---
+++ TaxCalculator/Program.cs
@@ -0,0 +1,26 @@
+﻿using CalculatingTax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                CalculateIncomeTax myTaxCalculator = new CalculateIncomeTax();
+
+                Console.WriteLine("Free Tax Calculator");
+                Console.Write("\nInput your monthly salary (GROSS) in AUD: ");
+                myTaxCalculator.grossSalary = double.Parse(Console.ReadLine());
+                
+                Console.WriteLine($"\nYour monthly salary (NET) after tax deduction in AUD: {myTaxCalculator.TotalSalary(myTaxCalculator.grossSalary)}");
+                
+
+                Console.ReadLine();
+        }
+    }
+}
